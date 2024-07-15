f6e53df09b55a062ef8a03678ff5dec619b42b25
Borewit <Borewit@users.noreply.github.com>
2024-07-15 21:50:08 +0200

    Explicit Nodejs type imports

--- lib/core.ts
+++ lib/core.ts
@@ -1,12 +1,13 @@
 import { ReadStreamTokenizer } from './ReadStreamTokenizer.js';
-import { Readable } from 'node:stream';
 import { BufferTokenizer } from './BufferTokenizer.js';
 import { IFileInfo } from './types.js';
 import { StreamReader, WebStreamReader} from 'peek-readable';
-import type { ReadableStream } from 'node:stream/web';
 export { EndOfStreamError } from 'peek-readable';
 export { ITokenizer, IFileInfo } from './types.js';
 export { IToken, IGetToken } from '@tokenizer/token';
+
+import type { Readable } from 'node:stream';
+import type { ReadableStream } from 'node:stream/web';
 
 /**
  * Construct ReadStreamTokenizer from given Stream.
